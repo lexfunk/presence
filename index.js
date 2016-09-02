20225a3f43dfb2bd9db92828fe53ab3108eb231d
Lex Vasquez <lex@ten7.com>
2016-09-02 15:19:41 -0500

    Added some code that might do something

--- index.js
+++ index.js
@@ -74,3 +74,35 @@ controller.hears(['attachment'], ['direct_message', 'direct_mention'], function 
 controller.hears('.*', ['direct_message', 'direct_mention'], function (bot, message) {
   bot.reply(message, 'Sorry <@' + message.user + '>, I don\'t understand. \n')
 })
+
+controller.on('slash_command', function (slashCommand, message) {
+
+    switch (message.command) {
+        case "/afk": //handle the `/echo` slash command. We might have others assigned to this app too!
+            // The rules are simple: If there is no text following the command, treat it as though they had requested "help"
+            // Otherwise just echo back to them what they sent us.
+
+            // but first, let's make sure the token matches!
+            if (message.token !== process.env.VERIFICATION_TOKEN) return; //just ignore it.
+
+            // if no text was supplied, treat it as a help command
+            if (message.text === "" || message.text === "help") {
+                slashCommand.replyPrivate(message,
+                    "I echo back what you tell me. " +
+                    "Try typing `/echo hello` to see.");
+                return;
+            }
+
+            // If we made it here, just echo what the user typed back at them
+            //TODO You do it!
+            slashCommand.replyPublic(message, "1", function() {
+                slashCommand.replyPublicDelayed(message, "2").then(slashCommand.replyPublicDelayed(message, "3"));
+            });
+
+            break;
+        default:
+            slashCommand.replyPublic(message, "I'm afraid I don't know how to " + message.command + " yet.");
+
+    }
+
+});
